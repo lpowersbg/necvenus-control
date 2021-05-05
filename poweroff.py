b1224a457bd693197fbeb95db74bbb147e4e91f0
lpowersbg <lpower@sbgtv.com>
2021-05-05 16:05:12 -0400

    Updated with current scripts, added monwall and venus module base

--- poweroff.py
+++ poweroff.py
@@ -1,22 +1,17 @@
 import socket
-import binascii
 
 def Main():
-    host = '192.168.2.3'
-    port = 8000
+    host = '192.168.0.1'
+    port = 7142
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    print 'connected'
-    message = '\x00\x01\x02\x01\x00\x12\x03\x6b\xd6\x6f\x01\x01\x00\x00\x02\x58\x69\x47\xae\x58\x69\x47\xae\x00'
-    s.send(message)
-    print 'sent command'
+    mon = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mon.connect((host, port))
+    message = b'\x01\x30\x2A\x30\x41\x30\x43\x02\x43\x32\x30\x33\x44\x36\x30\x30\x30\x34\x03\x1D\x0D'
+    mon.send(message)
+    exit()
 
-    data = s.recv(1024)
-    s.close()
-    print type(data)
-
-    print binascii.hexlify(data)
+    #ON message = b'\x01\x30\x2A\x30\x41\x30\x43\x02\x43\x32\x30\x33\x44\x36\x30\x30\x30\x31\x03\x18\x0D'
+    #OFF message = b'\x01\x30\x2A\x30\x41\x30\x43\x02\x43\x32\x30\x33\x44\x36\x30\x30\x30\x34\x03\x15\x0D'
 
 if __name__ == '__main__':
     Main()
